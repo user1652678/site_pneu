ca2415bf40a75830695049cf625202fd5d0cd7da
user1652678 <151149675+user1652678@users.noreply.github.com>
2023-12-08 09:13:10 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const imgs = document.getElementById("img");
+const img = document.querySelector("#img img");
+
+
+let idx = 0;
+ 
+
+function carrosel(){
+    idx++;
+      
+
+    if(idx > img.length - 1){
+        idx = 0;
+
+    }
+     
+    imgs.style.transform = 'translateX(${-idx * 500}px)';
+
+}
+
+setInterval(carrosel,1800);
